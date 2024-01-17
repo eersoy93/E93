3909b3800a59c66645d1d110d3e19ac1ee9f0dac
Erdem Ersoy <erdemersoy256@gmail.com>
2024-01-17 09:42:09 +0300

    Fix copyright years

--- src/drivers/speaker.c
+++ src/drivers/speaker.c
@@ -1,6 +1,6 @@
 /* DESCRIPTION: E93 PC Speaker Driver Source File
  * AUTHOR: Erdem Ersoy (eersoy93)
- * COPYRIGHT: Copyright (c) 2022-2024 Erdem Ersoy (eersoy93).
+ * COPYRIGHT: Copyright (c) 2024 Erdem Ersoy (eersoy93).
  * LICENSE: Licensed with MIT License. See LICENSE file for details.
  */
 
