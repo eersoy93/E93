b85d6ea23f3c6facfad928ea067b0aeb0ab28387
Erdem Ersoy <erdemersoy256@gmail.com>
2024-02-19 21:36:56 +0300

    Update code after video check

--- src/kernel/kernel_main.c
+++ src/kernel/kernel_main.c
@@ -19,7 +19,7 @@ void kernel_main(void)
 {
     clear_screen();
 
-    printk_color("Executing the kernel...\n", DEFAULT_COLOR);
+    printk_color("Executing the kernel...\n", OUTPUT_COLOR);
 
     if (is_video_colored() == TRUE)
     {
@@ -27,8 +27,6 @@ void kernel_main(void)
     }
     else
     {
-        printk_color("Your video card is NOT colored!\n", DEFAULT_COLOR);
-        printk_color("Colored video card is required!\n", DEFAULT_COLOR);
         end();
     }
 
