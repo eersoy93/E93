ad7e0e047bc41dafff963c42d2c833aa53c40fe9
Erdem Ersoy <erdemersoy256@gmail.com>
2024-05-16 19:56:02 +0300

    Add ide.h and ide.c stubs

---
+++ src/drivers/ide.c
@@ -0,0 +1,16 @@
+/* DESCRIPTION: E93 IDE Driver Source File
+ * AUTHOR: Erdem Ersoy (eersoy93)
+ * COPYRIGHT: Copyright (c) 2024 Erdem Ersoy (eersoy93).
+ * LICENSE: Licensed with MIT License. See LICENSE file for details.
+ */
+
+#include "ide.h"
+
+void ide_init(uint32_t BAR0, uint32_t BAR1, uint32_t BAR2, uint32_t BAR3, uint32_t BAR4)
+{
+    UNUSED(BAR0);
+    UNUSED(BAR1);
+    UNUSED(BAR2);
+    UNUSED(BAR3);
+    UNUSED(BAR4);
+}
---
+++ src/drivers/ide.h
@@ -0,0 +1,16 @@
+/* DESCRIPTION: E93 IDE Driver Header File
+ * AUTHOR: Erdem Ersoy (eersoy93)
+ * COPYRIGHT: Copyright (c) 2024 Erdem Ersoy (eersoy93).
+ * LICENSE: Licensed with MIT License. See LICENSE file for details.
+ */
+
+#ifndef IDE_H
+#define IDE_H
+
+#include "pci.h"
+
+#include <stdint.h>
+
+void ide_init(uint32_t BAR0, uint32_t BAR1, uint32_t BAR2, uint32_t BAR3, uint32_t BAR4);
+
+#endif
