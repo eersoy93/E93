72abd46f4c658332de4e0b3638040d2088836802
Erdem Ersoy <erdemersoy256@gmail.com>
2024-05-16 19:43:05 +0300

    Add declarations to pci.h

--- src/drivers/pci.h
+++ src/drivers/pci.h
@@ -25,5 +25,7 @@ struct pci_device_type
 
 uint16_t pci_config_read_word(uint8_t bus, uint8_t slot, uint8_t func, uint8_t offset);
 uint16_t pci_check_vendor(uint8_t bus, uint8_t slot);
+void pci_check_device(uint8_t bus, uint8_t slot, uint16_t vendor);
+void pci_check_all_devices(void);
 
 #endif
