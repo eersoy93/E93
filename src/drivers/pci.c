b31d70ab8437db30c83a822c939e12921a8ac990
Erdem Ersoy <erdemersoy256@gmail.com>
2024-05-17 03:16:48 +0300

    Revise the PCI code

--- src/drivers/pci.c
+++ src/drivers/pci.c
@@ -24,63 +24,127 @@ uint16_t pci_config_read_word(uint8_t bus, uint8_t slot, uint8_t func, uint8_t o
     return temp;
 }
 
-uint16_t pci_check_vendor(uint8_t bus, uint8_t slot)
+uint16_t pci_get_vendor_id(uint8_t bus, uint8_t slot)
 {
-    uint16_t vendor = 0x0000;
-    uint16_t device = 0x0000;
+    uint16_t vendor_id = pci_config_read_word(bus, slot, 0, 0);
 
-    if ((vendor = pci_config_read_word(bus, slot, 0, 0)) != 0xffff)
-    {
-        device = pci_config_read_word(bus, slot, 0, 2);
-        UNUSED(device);
-    }
-
-    return vendor;
+    return vendor_id;
 }
 
-void pci_check_device(uint8_t bus, uint8_t slot, uint16_t vendor)
+uint16_t pci_get_device_id(uint8_t bus, uint8_t slot)
 {
-    uint16_t device = pci_config_read_word(bus, slot, 0, 2);
+    uint16_t device_id = pci_config_read_word(bus, slot, 0, 2);
 
-    if (vendor != 0xffff)
+    return device_id;
+}
+
+struct pci_device_type_0 pci_get_information_0(uint8_t bus, uint8_t slot)
+{
+    uint16_t vendor_id = pci_get_vendor_id(bus, slot);
+
+    if (vendor_id != 0xffff)
     {
-        uint8_t pci_class_id = pci_config_read_word(bus, slot, 0, 0xa) & 0xff;
-        uint8_t pci_subclass_id = pci_config_read_word(bus, slot, 0, 0xa) >> 8;
+        uint16_t device_id = pci_get_device_id(bus, slot);
+        uint16_t status = pci_config_read_word(bus, slot, 0, 6);
+        uint16_t command = pci_config_read_word(bus, slot, 0, 4);
 
-        uint8_t pci_prog_if = pci_config_read_word(bus, slot, 0, 0x9) & 0xff;
+        uint8_t class_id = (pci_config_read_word(bus, slot, 0, 0xa) >> 8) & 0xff;
+        uint8_t subclass_id = pci_config_read_word(bus, slot, 0, 0xa) & 0xff;
 
-        uint8_t pci_header_type = pci_config_read_word(bus, slot, 0, 0xe) & 0xff;
+        uint8_t prog_if = (pci_config_read_word(bus, slot, 0, 8) >> 8) & 0xff;
+        uint8_t revision_id = pci_config_read_word(bus, slot, 0, 8) & 0xff;
 
-        if (pci_header_type == 0)
+
+        uint8_t bist = (pci_config_read_word(bus, slot, 0, 0xe) >> 8) & 0xff;
+        uint8_t header_type = pci_config_read_word(bus, slot, 0, 0xe) & 0xff;
+
+        uint8_t latency_timer = (pci_config_read_word(bus, slot, 0, 0x0c) >> 8) & 0xff;
+        uint8_t cache_line_size = pci_config_read_word(bus, slot, 0, 0x0c) & 0xff;
+
+        if (header_type == 0)
         {
-            uint8_t irq = pci_config_read_word(bus, slot, 0, 0x3c) & 0xff;
+            uint16_t BAR0_low = pci_config_read_word(bus, slot, 0, 0x10);
+            uint16_t BAR0_high = pci_config_read_word(bus, slot, 0, 0x12);
 
-            struct pci_device_type pci_device =
+            uint16_t BAR1_low = pci_config_read_word(bus, slot, 0, 0x14);
+            uint16_t BAR1_high = pci_config_read_word(bus, slot, 0, 0x16);
+
+            uint16_t BAR2_low = pci_config_read_word(bus, slot, 0, 0x18);
+            uint16_t BAR2_high = pci_config_read_word(bus, slot, 0, 0x1a);
+
+            uint16_t BAR3_low = pci_config_read_word(bus, slot, 0, 0x1c);
+            uint16_t BAR3_high = pci_config_read_word(bus, slot, 0, 0x1e);
+
+            uint16_t BAR4_low = pci_config_read_word(bus, slot, 0, 0x20);
+            uint16_t BAR4_high = pci_config_read_word(bus, slot, 0, 0x22);
+
+            uint16_t BAR5_low = pci_config_read_word(bus, slot, 0, 0x24);
+            uint16_t BAR5_high = pci_config_read_word(bus, slot, 0, 0x26);
+
+            uint16_t cardbus_cis_pointer_low = pci_config_read_word(bus, slot, 0, 0x28);
+            uint16_t cardbus_cis_pointer_high = pci_config_read_word(bus, slot, 0, 0x2a);
+
+            uint16_t subsystem_vendor_id = pci_config_read_word(bus, slot, 0, 0x2c);
+            uint16_t subsystem_id = pci_config_read_word(bus, slot, 0, 0x2e);
+
+            uint16_t expansion_rom_base_address_low = pci_config_read_word(bus, slot, 0, 0x30);
+            uint16_t expansion_rom_base_address_high = pci_config_read_word(bus, slot, 0, 0x32);
+
+            uint8_t capabilities_pointer = pci_config_read_word(bus, slot, 0, 0x34) & 0xff;
+
+            uint8_t interrupt_line = pci_config_read_word(bus, slot, 0, 0x3c) & 0xff;
+            uint8_t interrupt_pin = (pci_config_read_word(bus, slot, 0, 0x3c) >> 8) & 0xff;
+            uint8_t min_grant = pci_config_read_word(bus, slot, 0, 0x3e) & 0xff;
+            uint8_t max_latency = (pci_config_read_word(bus, slot, 0, 0x3e) >> 8) & 0xff;
+
+            struct pci_device_type_0 pci_device =
             {
-                .bus = bus,
-                .slot = slot,
-                .vendor = vendor,
-                .device = device,
-                .pci_class_id = pci_class_id,
-                .pci_subclass_id = pci_subclass_id,
-                .pci_prog_if = pci_prog_if,
-                .irq = irq
+                device_id,
+                vendor_id,
+                status,
+                command,
+                class_id,
+                subclass_id,
+                prog_if,
+                revision_id,
+                bist,
+                header_type,
+                latency_timer,
+                cache_line_size,
+                BAR0_low,
+                BAR0_high,
+                BAR1_low,
+                BAR1_high,
+                BAR2_low,
+                BAR2_high,
+                BAR3_low,
+                BAR3_high,
+                BAR4_low,
+                BAR4_high,
+                BAR5_low,
+                BAR5_high,
+                cardbus_cis_pointer_low,
+                cardbus_cis_pointer_high,
+                subsystem_vendor_id,
+                subsystem_id,
+                expansion_rom_base_address_low,
+                expansion_rom_base_address_high,
+                capabilities_pointer,
+                interrupt_line,
+                interrupt_pin,
+                min_grant,
+                max_latency
             };
 
-            UNUSED(pci_device);
+            return pci_device;
+        }
+        else
+        {
+            return (struct pci_device_type_0){0};
         }
     }
-}
-
-void pci_check_all_devices(void)
-{
-    for (uint8_t bus = 0; bus < 8; bus++)
+    else
     {
-        for (uint8_t slot = 0; slot < 32; slot++)
-        {
-            uint16_t vendor = pci_check_vendor(bus, slot);
-
-            pci_check_device(bus, slot, vendor);
-        }
+        return (struct pci_device_type_0){0};
     }
 }
--- src/drivers/pci.h
+++ src/drivers/pci.h
@@ -10,22 +10,50 @@
 #include <stddef.h>
 #include <stdint.h>
 
-struct pci_device_type
+struct pci_device_type_0
 {
-    uint8_t bus;
-    uint8_t slot;
-    uint16_t vendor;
-    uint16_t device;
-    uint8_t pci_class_id;
-    uint8_t pci_subclass_id;
-    uint8_t pci_prog_if;
-    uint8_t pci_header_type;
-    uint8_t irq;
+    uint16_t device_id;
+    uint16_t vendor_id;
+    uint16_t status;
+    uint16_t command;
+    uint8_t class_id;
+    uint8_t subclass_id;
+    uint8_t prog_if;
+    uint8_t revision_id;
+    uint8_t bist;
+    uint8_t header_type;
+    uint8_t latency_timer;
+    uint8_t cache_line_size;
+    uint16_t BAR_O_low;
+    uint16_t BAR_O_high;
+    uint16_t BAR_1_low;
+    uint16_t BAR_1_high;
+    uint16_t BAR_2_low;
+    uint16_t BAR_2_high;
+    uint16_t BAR_3_low;
+    uint16_t BAR_3_high;
+    uint16_t BAR_4_low;
+    uint16_t BAR_4_high;
+    uint16_t BAR_5_low;
+    uint16_t BAR_5_high;
+    uint16_t cardbus_cis_pointer_low;
+    uint16_t cardbus_cis_pointer_high;
+    uint16_t subsystem_vendor_id;
+    uint16_t subsystem_id;
+    uint16_t expansion_ROM_base_address_low;
+    uint16_t expansion_ROM_base_address_high;
+    uint8_t capabilities_pointer;
+    uint8_t interrupt_line;
+    uint8_t interrupt_pin;
+    uint8_t min_grant;
+    uint8_t max_latency;
 };
 
 uint16_t pci_config_read_word(uint8_t bus, uint8_t slot, uint8_t func, uint8_t offset);
-uint16_t pci_check_vendor(uint8_t bus, uint8_t slot);
-void pci_check_device(uint8_t bus, uint8_t slot, uint16_t vendor);
-void pci_check_all_devices(void);
+
+uint16_t pci_get_vendor_id(uint8_t bus, uint8_t slot);
+uint16_t pci_get_device_id(uint8_t bus, uint8_t slot);
+
+struct pci_device_type_0 pci_get_information_0(uint8_t bus, uint8_t slot);
 
 #endif
