4f8e476cc580e7702a170559bbc8158c2dff43d9
Erdem Ersoy <erdemersoy256@gmail.com>
2024-05-17 14:37:48 +0300

    Update file headers

--- src/libc/init.c
+++ src/libc/init.c
@@ -1,4 +1,4 @@
-/* DESCRIPTION: E93 Shell Init Source File
+/* DESCRIPTION: E93 C Library Shell Init Function Source File
  * AUTHOR: Erdem Ersoy (eersoy93)
  * COPYRIGHT: Copyright (c) 2024 Erdem Ersoy (eersoy93).
  * LICENSE: Licensed with MIT License. See LICENSE file for details.
