e8d44595e081afc187ea7205fe4c0a9d085d89e8
Erdem Ersoy <erdemersoy256@gmail.com>
2024-08-03 19:32:31 +0300

    Implement (?) ide_init() with GitHub Copilot

--- src/drivers/ide.c
+++ src/drivers/ide.c
@@ -6,6 +6,7 @@
 
 #include "../cpu/drvutils.h"
 #include "../cpu/ports.h"
+#include "../cpu/timer.h"
 #include "screen.h"
 
 #include "ide.h"
@@ -268,9 +269,149 @@ uint8_t ide_print_error(uint32_t drive, uint8_t error)
 
 void ide_init(uint32_t BAR0, uint32_t BAR1, uint32_t BAR2, uint32_t BAR3, uint32_t BAR4)
 {
-    UNUSED(BAR0);
-    UNUSED(BAR1);
-    UNUSED(BAR2);
-    UNUSED(BAR3);
-    UNUSED(BAR4);
+    // Detect IDE Channels
+
+    IDEChannels[ATA_PRIMARY].base = (BAR0 == 0) ? 0x1F0 : BAR0;
+    IDEChannels[ATA_PRIMARY].ctrl = (BAR1 == 0) ? 0x3F6 : BAR1;
+    IDEChannels[ATA_PRIMARY].bmide = (BAR4 == 0) ? 0 : BAR4;
+    IDEChannels[ATA_PRIMARY].nIEN = 0;
+
+    IDEChannels[ATA_SECONDARY].base = (BAR2 == 0) ? 0x170 : BAR2;
+    IDEChannels[ATA_SECONDARY].ctrl = (BAR3 == 0) ? 0x376 : BAR3;
+    IDEChannels[ATA_SECONDARY].bmide = (BAR4 == 0) ? 0 : BAR4;
+    IDEChannels[ATA_SECONDARY].nIEN = 0;
+
+    // Disable IRQs
+    ide_write(ATA_PRIMARY, ATA_REG_CONTROL, 2);
+    ide_write(ATA_SECONDARY, ATA_REG_CONTROL, 2);
+
+    // Detect IDE Devices
+    for (uint8_t i = 0; i < 2; i++)
+    {
+        for (uint8_t j = 0; j < 2; j++)
+        {
+            uint8_t err = 0;
+            uint8_t type = IDE_ATA;
+            uint8_t status = 0;
+
+            IDEDevices[ATA_MASTER].Reserved = 0;
+            IDEDevices[ATA_SLAVE].Reserved = 0;
+
+            // Select Drive
+            ide_write(i, ATA_REG_HDDEVSEL, 0xA0 | (j << 4));
+            wait_timer(100);
+
+            // Send ATA Identify Command
+            ide_write(i, ATA_REG_COMMAND, ATA_CMD_IDENTIFY);
+            wait_timer(100);
+
+            // Polling
+            if (ide_read(i, ATA_REG_STATUS) == 0)
+            {
+                continue;
+            }
+
+            while (1)
+            {
+                status = ide_read(i, ATA_REG_STATUS);
+                if ((status & ATA_SR_ERR))
+                {
+                    err = 1;
+                    break;
+                }
+                if (!(status & ATA_SR_BSY) && (status & ATA_SR_DRQ))
+                {
+                    break;
+                }
+            }
+
+            // Check if device is ATAPI
+            if (err != 0)
+            {
+                uint8_t cl = ide_read(i, ATA_REG_LBA1);
+                uint8_t ch = ide_read(i, ATA_REG_LBA2);
+
+                if (cl == 0x14 && ch == 0xEB)
+                {
+                    type = IDE_ATAPI;
+                }
+                else if (cl == 0x69 && ch == 0x96)
+                {
+                    type = IDE_ATAPI;
+                }
+                else
+                {
+                    continue;
+                }
+
+                ide_write(i, ATA_REG_COMMAND, ATA_CMD_IDENTIFY_PACKET);
+                wait_timer(100);
+            }
+
+            // Read Identification Space of the Device
+            ide_read_buffer(i, ATA_REG_DATA, (uint32_t)IDEDevices[i * 2 + j].Model, 40);
+
+            // Read Device Parameters
+            ide_read_buffer(i, ATA_REG_DATA, (uint32_t)&IDEDevices[i * 2 + j].Capabilities, 1);
+            ide_read_buffer(i, ATA_REG_DATA, (uint32_t)&IDEDevices[i * 2 + j].CommandSets, 1);
+            ide_read_buffer(i, ATA_REG_DATA, (uint32_t)&IDEDevices[i * 2 + j].Type, 1);
+            ide_read_buffer(i, ATA_REG_DATA, (uint32_t)&IDEDevices[i * 2 + j].Signature, 1);
+            ide_read_buffer(i, ATA_REG_DATA, (uint32_t)&IDEDevices[i * 2 + j].Size, 2);
+
+            // Print Device Information
+            if (IDEDevices[i * 2 + j].Type == 0)
+            {
+                continue;
+            }
+
+            if (type == IDE_ATA)
+            {
+                if (IDEDevices[i * 2 + j].Type == 0x00)
+                {
+                    continue;
+                }
+
+                printl_color("Found ATA Device: ", OUTPUT_COLOR);
+            }
+            else if (type == IDE_ATAPI)
+            {
+                printl_color("Found ATAPI Device: ", OUTPUT_COLOR);
+            }
+
+            char ide_device_number_str[2] = { 0 };
+
+            int_to_ascii(i * 2 + j, ide_device_number_str);
+
+            printl_color(ide_device_number_str, OUTPUT_COLOR);
+
+            printl_color("\n", OUTPUT_COLOR);
+
+            char ide_device_model_str[41] = { 0 };
+
+            for (uint8_t k = 0; k < 40; k += 2)
+            {
+                ide_device_model_str[k] = IDEDevices[i * 2 + j].Model[k + 1];
+                ide_device_model_str[k + 1] = IDEDevices[i * 2 + j].Model[k];
+            }
+
+            printl_color("Model: ", OUTPUT_COLOR);
+            printl_color(ide_device_model_str, OUTPUT_COLOR);
+
+            printl_color("\n", OUTPUT_COLOR);
+
+            char ide_device_size_str[10] = { 0 };
+
+            int_to_ascii(IDEDevices[i * 2 + j].Size, ide_device_size_str);
+
+            printl_color("Size: ", OUTPUT_COLOR);
+
+            printl_color(ide_device_size_str, OUTPUT_COLOR);
+
+            printl_color(" MB\n", OUTPUT_COLOR);
+
+            printl_color("\n", OUTPUT_COLOR);
+        }
+    }
+
+    printl_color("IDE Initialization Completed!\n", OUTPUT_COLOR);
 }
