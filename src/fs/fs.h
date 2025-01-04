f2877e6bb9524688a30d28754d7b3c8c7af9da12
Erdem Ersoy <erdemersoy256@gmail.com>
2025-01-04 21:01:18 +0300

    Add list of open files definiton

---
+++ src/fs/fs.h
@@ -0,0 +1,14 @@
+/* DESCRIPTION: E93 IS09660 File System Header File
+ * AUTHOR: Erdem Ersoy (eersoy93)
+ * COPYRIGHT: Copyright (c) 2025 Erdem Ersoy (eersoy93).
+ * LICENSE: Licensed with MIT License. See LICENSE file for details.
+ */
+
+#ifndef FS_H
+#define FS_H
+
+#include <stdint.h>
+
+int8_t open_files_list[127] = { 0 };
+
+#endif
